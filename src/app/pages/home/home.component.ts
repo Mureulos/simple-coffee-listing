0f0fb7cf801a40524ca7ed425762018ec5ebf50f
mureulos <murilodesantana05@gmail.com>
2024-07-08 09:27:27 -0300

    função de filtragem implementada nos botões

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,28 +1,36 @@
-//app/pages/home/home.component.ts
 import { Component, Input, OnInit } from '@angular/core';
 import { CoffeeListService } from '../../shared/services/coffee-list.service';
 import { CoffeeType } from '../../shared/interfaces/type';
-import { provideHttpClient } from '@angular/common/http';
 import { CardCoffeeComponent } from '../../shared/components/card-coffee/card-coffee.component';
 import { content } from '../../../assets/content/content';
 import { FilterButtonComponent } from '../../shared/components/filter-button/filter-button.component';
-
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CardCoffeeComponent, FilterButtonComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  contentAplication = content
+  contentAplication = content;
   data: CoffeeType[] = [];
+
   constructor(private coffeeListService: CoffeeListService) {}
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
     this.coffeeListService.getData().subscribe((response) => {
       this.data = response;
-      console.log(this.data);
+    });
+  }
+
+  filterData(): void {
+    this.coffeeListService.filterData().subscribe((response) => {
+      console.log(response)
+      this.data = response;
     });
   }
 }
--- src/app/shared/components/filter-button/filter-button.component.ts
+++ src/app/shared/components/filter-button/filter-button.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-filter-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter-button.component.html',
   styleUrl: './filter-button.component.scss'
 })
 export class FilterButtonComponent {
+  @Input() text: string = ''
+  @Input() active: boolean = false
+  @Input() action?: () => void
 
+  onClick(): void {
+    if (this.action) {
+      this.action();
+    }
+  }
 }
